64c0010d09032953161755b4246a8c775c2917a8
Manoj Phaju <manojphaju@manojs-mbp.home>
2023-05-08 17:44:05 +0100

    MY_SECRET_TOKEN added

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,5 +1,11 @@
+import os
+
 def main():
     print('Hello from python script ')
+    token = os.environ.get('MY_SECRET_TOKEN')
+    if not token:
+        RuntimeError('token not found')
+    print('YEAH! all good. Token found.')
 
 
 if __name__ == '__main__':
